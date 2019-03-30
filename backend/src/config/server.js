3ed2e4eb0437acf43f9f139196710965f39f5613
TrindadeThiago <thiago.trindadexd@gmail.com>
2019-03-30 16:32:40 -0300

    pequenas correções de sintaxe

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -7,7 +7,7 @@ const server = express()
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 
-server.listen({host:'0.0.0.0',port:port}, function() {
+server.listen(port, function() {
     console.log(`BACKEND is running on port ${port}.`)
 })
 
