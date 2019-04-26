e0460179190498c710bc8009880f4305e7a07f8f
TrindadeThiago <thiago.trindadexd@gmail.com>
2019-04-25 21:37:58 -0300

    Conexão do front com o backend (erro na requisição)

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,6 +1,7 @@
 module.exports = function(req, res, next) {
-    res.header('Acess-Control-Allow-Origin', '*')
-    res.header('Acess-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    res.header('Acess-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    console.log(req.headers)
+    res.headers('Access-Control-Allow-Origin', '*')
+    res.headers('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.headers('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     next()
-}
+}   
